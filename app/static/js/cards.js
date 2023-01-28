26b8b2e66ad2494860ea105efec72d76457b8cf6
Tami Takada <tamitakada@gmail.com>
2023-01-28 16:34:30 -0500

    support for d3 based hierarchy

--- app/static/js/cards.js
+++ app/static/js/cards.js
@@ -80,6 +80,7 @@ function adjustPageSize() {
 // HIERARCHY DIAGRAM SET UP ===========================================================
 
 let loadedMemos = [];
+let selectedNode = null;
 
 function loadAllDiagramsForMemo(memoIndex) {
     if (!loadedMemos.includes(memoIndex)) {
@@ -93,64 +94,124 @@ function parseHierarchyData(hierarchyDataString) {
     let nodeData = hierarchyDataString.split("+");
     let hierarchyData = [];
     for (let i = 0; i < nodeData.length; i++) {
-        console.log(nodeData[i]);
         hierarchyData.push(JSON.parse(nodeData[i]));
     }
     return hierarchyData;
 }
 
 function loadDiagram(diagram) {
-    const diagramContainer = diagram.getElementsByClassName("diagram")[0];
-    const diagramCaption = diagram.getElementsByClassName("diagram-caption")[0];
-    const diagramHeading = diagram.getElementsByClassName("diagram-heading")[0];
-    const diagramExplanation = diagram.getElementsByClassName("diagram-explanation")[0];
+    const diagramContainer = diagram.getElementsByClassName("hierarchy-diagram")[0];
+    const nodeCaption = diagram.getElementsByClassName("node-caption")[0];
+    const nodeCaptionHeading = diagram.getElementsByClassName("node-caption-heading")[0];
+    const nodeCaptionText = diagram.getElementsByClassName("node-caption-text")[0];
 
-    const hierarchy = new go.Diagram(diagramContainer, { layout: new go.TreeLayout({ 
-        angle: 90, 
-        layerSpacing: 35, 
-        arrangement: go.TreeLayout.ArrangementHorizontal}),
-        allowVerticalScroll: false,
-        allowHorizontalScroll: false,
-        autoScrollRegion: 0,
-        "panningTool.isEnabled": false,
-        "dragSelectingTool.isEnabled": false,
-        initialScale: 1.2
-    });
+    const data = parseHierarchyData(diagram.getAttribute("data-hierarchy"));
+    
+    for (let i = 0; i < data.length; i++) {
+        var svg = d3.select(diagramContainer)
+            .append("svg")
+            .attr("width", "" + (100 * data[i]["width"]) + "%")
+            .attr("height", data[i]["height"])
+            .attr("viewBox", function() {
+                let width = parseInt(d3.select(this).style("width"));
+                let height = parseInt(d3.select(this).style("height"));
+                return "0 0 " + width + " " + height;
+            }),
+            g = svg.append("g")
+                .attr("transform", "translate(0,70)");
 
-    hierarchy.nodeTemplate = new go.Node("Auto", {
-        selectionChanged: function(node) {
-            if (node.data.desc) {
-                diagramCaption.style.display = "flex";
-                diagramExplanation.innerText = node.data.desc;
-                diagramHeading.innerText = node.data.name;
-            } else { diagramCaption.style.display = "none"; }
-        }
-    })
-        .bind("selectable", "tooltip")
-        .add(new go.Shape("RoundedRectangle",
-            { strokeWidth: 3, stroke: "black", name: "SHAPE", fill: "#384669" })
-            .bind("stroke", "color"))
-        .add(new go.Panel("Vertical", { padding: 8 })
-            .add(new go.TextBlock("",
-                { font: "8px Hina", stroke: "white", name: "RUBY" })
-                .bind("text", "furigana")
-                .bind("visible", "furigana", function(text) { 
-                    console.log(text);
-                    return (text.length > 0); 
-                }))
-            .add(new go.TextBlock("",
-                { font: "14px Hina", stroke: "white", wrap: go.TextBlock.WrapBreakAll, maxSize: new go.Size(70, 100), name: "TEXT" })
-                .bind("text", "name")));
+        let width = svg.node().getBoundingClientRect().width;
 
-    hierarchy.linkTemplate = new go.Link({ routing: go.Link.Orthogonal, corner: 5, selectable: false })
-        .add(new go.Shape({ strokeWidth: 2, stroke: "white" }))
+        var tree = d3.tree()
+            .size([width, data[i]["height"] - 120])
 
-    hierarchyData = parseHierarchyData(diagram.getAttribute("data-hierarchy"));
-    hierarchy.model = new go.TreeModel(hierarchyData);
+        let root = d3.hierarchy(data[i]["nodeData"]);
+        tree(root);
 
-    hierarchy.isReadOnly = true;
-    hierarchy.maxSelectionCount = 1;
-    hierarchy.allowZoom = true;
+        let link = g.selectAll(".link")
+            .data(root.descendants().slice(1))
+            .enter().append("path")
+                .attr("class", "link")
+                .attr("d", function(d) {
+                return "M" + d.x + "," + d.y
+                    + "C" + d.x + "," + (d.y + d.parent.y) / 2
+                    + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
+                    + " " + d.parent.x + "," + d.parent.y;
+                });
+
+        let node = g.selectAll(".node")
+            .data(root.descendants())
+            .enter().append("g")
+                .attr("class", function(d) {
+                    return "node" + (d.children ? " node--internal" : " node--leaf");
+                })
+                .attr("transform", function(d) {
+                    return "translate(" + d.x + "," + d.y + ")";
+                });
+
+        let text = node.append("text")
+            .text(function(d) { return d.data.name; })
+            .attr("dy", ".35em")
+            .attr("y", function(d) {
+                return d.children ? -20 : 20;
+            })
+            .style("text-anchor", "middle")
+            .style("stroke", "white");
+
+        let rect = node.insert("rect", "text")
+            .attr("x", function(d) {
+                return d3.select(this.parentNode).select("text").node().getBBox().x - 10;
+            })
+            .attr("y", function(d) {
+                if (d.data.ruby) {
+                    return d3.select(this.parentNode).select("text").node().getBBox().y - 15;
+                }
+                return d3.select(this.parentNode).select("text").node().getBBox().y - 10;
+            })
+            .attr("width", function(d) {
+                return d3.select(this.parentNode).select("text").node().getBBox().width + 20;
+            })
+            .attr("height", function(d) {
+                if (d.data.ruby) {
+                    return d3.select(this.parentNode).select("text").node().getBBox().height + 25;
+                }
+                return d3.select(this.parentNode).select("text").node().getBBox().height + 20;
+            })
+            .attr("rx", 5)
+            .attr("ry", 5)
+            .attr("class", "node-text")
+            .style("fill", "var(--lightNavy)")
+            .style("stroke", "white")
+            .style("stroke-width", 2)
+        
+        let ruby = node.append("text")
+            .text(function(d) { return d.data.ruby; })
+            .attr("y", function(d) {
+                return d3.select(this.parentNode).select("text").node().getBBox().y;
+            })
+            .attr("class", "node-ruby")
+            .style("text-anchor", "middle")
+            .style("stroke", "white");
+
+        node.on("click", function(d) {
+            if (d.data.tooltip) {
+                if (selectedNode) {
+                    d3.select(selectedNode).select("rect").transition()
+                        .duration(200)
+                        .style("stroke", "white");
+                }
+                
+                selectedNode = this;
+                d3.select(this).select("rect").transition()
+                    .duration(200)
+                    .style("stroke", "var(--sea)");
+
+                nodeCaption.style.display = "block";
+                nodeCaptionHeading.innerText = d.data.name;
+                nodeCaptionText.innerText = d.data.tooltip;
+            }
+        });
+    }
 }
 
 window.addEventListener("resize", adjustPageSize, true);
